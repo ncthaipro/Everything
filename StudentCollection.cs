2a422a9cfa30f146a51947fc5f24c6a5a7daadde
NCThai <ncthai.1159@vitenet.net>
2022-09-16 17:35:25 +0700

    commit student collection

---
+++ StudentCollection.cs
@@ -0,0 +1,9 @@
+// Student collection
+IList<Student> studentList = new List<Student>() 
+{ 
+	new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+	new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 },
+	new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+	new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+	new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 } 
+};
