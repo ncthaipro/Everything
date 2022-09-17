a5bab9c6cfa599732456c9939d0aceb0ae0c274d
ncthai <90391027+ncthai@users.noreply.github.com>
2022-09-17 22:09:38 +0700

    commit change list

--- StudentCollection.cs
+++ StudentCollection.cs
@@ -5,5 +5,5 @@ IList<Student> studentList = new List<Student>()
 	new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 },
 	new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
 	new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
-	new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 } 
-};
+	new Student() { StudentID = 5, StudentName = "Knight" , Age = 19 } 
+}; 
