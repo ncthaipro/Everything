16e833f1a37bd38bc75bc2d174e3d6ab7d9b9af6
NCThai <ncthai.1159@vitenet.net>
2022-09-19 11:47:36 +0700

    update steps

--- .netcore.cs
+++ .netcore.cs
@@ -4,9 +4,10 @@ step 2: Check everything installed correctly : > dotnet
 step 3: Create your app : > dotnet new console -o MyApp -f net6.0
 step 4: > cd MyApp
 step 5: > code .
-step 6: > dotnet run
+step 6: > dotnet run (using at vscode)
 -----create-----
 step 7: > dotnet new mvc -au None -o aspnetapp
 step 8: > cd aspnetapp
 step 9: > code .
+step 10: > dotnet run
 done
