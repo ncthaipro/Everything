e97c5c2b725d6e2b86f78d67685ff31519e05ab8
NCThai <ncthai.1159@vitenet.net>
2022-09-19 11:23:57 +0700

    add .netcore.cs

---
+++ .netcore.cs
@@ -0,0 +1,7 @@
+start
+step 1:  Download and install 
+step 2: Check everything installed correctly : > dotnet
+step 3: Create your app : > dotnet new console -o MyApp -f net6.0
+step 4: > cd MyApp
+step 5: > dotnet run
+done
