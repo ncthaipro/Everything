922c8c67f6f77e382f6ce28d507263626e08c795
NCThai <ncthai.1159@vitenet.net>
2022-09-19 11:27:18 +0700

    update step 5

--- .netcore.cs
+++ .netcore.cs
@@ -1,7 +1,8 @@
-start
+start https://dotnet.microsoft.com/en-us/learn/dotnet/hello-world-tutorial/next
 step 1:  Download and install 
 step 2: Check everything installed correctly : > dotnet
 step 3: Create your app : > dotnet new console -o MyApp -f net6.0
 step 4: > cd MyApp
-step 5: > dotnet run
+step 5: > code .
+step 6: > dotnet run
 done
