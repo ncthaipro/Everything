40bc5fc38aa5f2589957cfb531a8c2425391f3ec
NCThai <ncthai.1159@vitenet.net>
2022-09-19 11:41:52 +0700

    update steps to create aspnetapp

--- .netcore.cs
+++ .netcore.cs
@@ -5,4 +5,8 @@ step 3: Create your app : > dotnet new console -o MyApp -f net6.0
 step 4: > cd MyApp
 step 5: > code .
 step 6: > dotnet run
+-----create-----
+step 7: > dotnet new mvc -au None -o aspnetapp
+step 8: > cd aspnetapp
+step 9: > code .
 done
