ff1e8795a95be3fae439f1ee188fa19f93a22e38
NCThai <113668489+ncthaipro@users.noreply.github.com>
2022-09-20 23:14:45 +0700

    Update DisableEventsOfDOMElementsWithJS.js

--- DisableEventsOfDOMElementsWithJS.js
+++ DisableEventsOfDOMElementsWithJS.js
@@ -1,1 +1,5 @@
-
+//source: https://stackoverflow.com/questions/8249641/enable-disable-events-of-dom-elements-with-js-jquery
+//demo: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_click
+ $("#link2").click(function() {
+          $("#link1").toggleClass("disabled");
+ });
