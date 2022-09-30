cb7522769eed061fc3ed360187b9f641664dcb2b
NCThai <113668489+ncthaipro@users.noreply.github.com>
2022-09-30 13:39:04 +0700

    Create CheckExists.cs

---
+++ CheckExists.cs
@@ -0,0 +1,10 @@
+//Check value exists in list
+var listCheck = new List<string>();
+listCheck.Add("1159");
+listCheck.Add("1160");
+listCheck.Add("1161");
+
+if (listCheck.Contains("1111")) // return > false . Check value exists in list
+{
+    // hander code
+}
