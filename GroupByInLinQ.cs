ef6b9a0b7e692f82497fcd91d124bdc12705c6a6
NCThai <113668489+ncthaipro@users.noreply.github.com>
2022-09-30 10:48:02 +0700

    Delete GroupByInLinQ.cs

--- GroupByInLinQ.cs
+++
@@ -1,4 +0,0 @@
-var results= persons.GroupBy(n => new { n.PersonId, n.car})
-                .Select(g => new {
-                               g.Key.PersonId,
-                               g.Key.car)}).ToList();
